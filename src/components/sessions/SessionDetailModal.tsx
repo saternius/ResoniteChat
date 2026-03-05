c611c1a568b7061da7e7fa47278c974bccac0b71
saternius <satern693@gmail.com>
2026-03-04 21:53:39 -0600

    higher session panel

--- src/components/sessions/SessionDetailModal.tsx
+++ src/components/sessions/SessionDetailModal.tsx
@@ -50,7 +50,7 @@ export function SessionDetailModal({
 
   return (
     <div
-      className="fixed inset-0 bg-black/60 z-50 flex items-center justify-center p-4 animate-fade-in"
+      className="fixed inset-0 bg-black/60 z-50 flex items-start justify-center pt-[10vh] p-4 animate-fade-in"
       onClick={onClose}
     >
       <div
